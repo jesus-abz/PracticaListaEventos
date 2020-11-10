f866e1506c1f53f935551006ea6f2d8a3c2ca0e7
Alumno <alumno@correo.com>
2020-11-10 13:15:25 -0700

    Initial Commit

---
+++ ListaEventos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListaEventos
+//
+//  Created by Alumno on 11/10/20.
+//  Copyright © 2020 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
